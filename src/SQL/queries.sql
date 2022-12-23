427d7b291ac816a0d205cc0378dd6e6972ed73cf
hozlucas28 <hozlucas28@gmail.com>
2022-12-22 21:42:34 -0300

    Update 'queries.sql' file

--- src/SQL/queries.sql
+++ src/SQL/queries.sql
@@ -1,61 +1,55 @@
-/* --------------------------------------------------------------------------
- * APUNTES:
- * 		   Estas instrucciones son ejecutadas directamente desde el gestor de
- *		   bases de datos, en el apartado de consultas.
- *		   Tener en cuenta que la variable seguida de la instrucción <TABLE>
- *		   indica la tabla a apuntar donde harán efecto las instrucciones consecuentes.
- *
- *
- * IMPORTANTE:
- *  			  - <Ctrl + f9> = ejecuta la/s instrucción/es seleccionada/s.
- * 				  - <ALTER> = indica la alteración de la tabla consecuente.
--------------------------------------------------------------------------- */
+/* ------------------------------ Base De Datos ----------------------------- */
 
-/* -------------------------- Instrucciones Básicas ------------------------- */
-
--- Crear una tabla llamada 'Music' y sus filas.
-CREATE TABLE Music (
-	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Fila única e irrepetible por la función 'AUTO_INCREMENT'
-	articleID INT NOT NULL,
-	label VARCHAR(50) NOT NULL,
-	theCode BIGINT NOT NULL,
-	theFormat VARCHAR(50) NOT NULL,
-	classification VARCHAR(100) NOT NULL	
-)
-
-ALTER TABLE Article ADD COLUMN pages INT NOT NULL; -- Agrega una fila de tipo entero no nulo.
-ALTER TABLE Article ADD COLUMN editorial VARCHAR(100); -- Agrega una fila de tipo string de 100 caracteres.
-
-DROP TABLE ArticleTwo; -- Elimina la tabla 'ArticleTwo'.
-ALTER TABLE Article DROP COLUMN ISBN; -- Elimina la fila 'ISBN'.
-
-ALTER TABLE Book ADD CONSTRAINT CHECK(numberPages > 0) -- Agrega una condición a la fila 'numberPages'.
+CREATE DATABASE webapp_practice CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+USE webapp_practice;
 
 
+/* --------------------------------- Tablas --------------------------------- */
 
-/* ------------------------- LLaves y Vinculaciones ------------------------- */
+CREATE TABLE Employees (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	firstName VARCHAR(31) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+	lastName VARCHAR(31) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+	telephone VARCHAR(31) NOT NULL,
+	workstation VARCHAR(127) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+	salary DECIMAL(11, 2) NOT NULL,
+	startDate DATE DEFAULT(CURRENT_DATE) NOT NULL,
+	endDate DATE,
+	isActive BOOL NOT NULL
+);
 
-ALTER TABLE Book ADD CONSTRAINT Book__articleID__unique UNIQUE(articleID) -- Define una llave 'UNIQUE' a la fila 'articleId' de la tabla 'Book'.
+CREATE TABLE Products (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(31) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+	brand VARCHAR(31) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+	price DECIMAL(11, 2) NOT NULL,
+	stock INT NOT NULL,
+	entryDate DATE DEFAULT(CURRENT_DATE) NOT NULL,
+	expireDate DATE
+);
 
--- Vincular la fila 'musicID' (tabla 'MusicThemes') al 'ID' (tabla 'Music') - Llave 'FOREIGN'.
-ALTER TABLE	MusicThemes ADD CONSTRAINT MusicThemes__articleID__Article__foreignKey FOREIGN KEY(musicID) REFERENCES Music(ID);
+
+/* -------------------------------- Registros ------------------------------- */
+
+INSERT INTO Employees(firstName, lastName, telephone, workstation, salary, startDate, endDate, isActive)
+	VALUES ('Marcos', 'Gonzalez', '1122436123', 'Reponedor', 52000, '2021-08-21', '2022-02-26', 0);
+
+INSERT INTO Employees(firstName, lastName, telephone, workstation, salary, startDate, isActive)
+	VALUES
+		('Lucas', 'Hoz', '1133476826', 'Gerente', 120000, '2018-03-17', 1),
+		('Juana', 'Hernandez', '1124356122', 'Cajera', 64000, '2022-03-14', 1);
+
+INSERT INTO Products(name, brand, price, stock, entryDate, expireDate)
+	VALUES 
+		('Oreos', 'Mondelez International, Inc.', 149.99, 180, '2022-11-27', '2023-05-27'),
+		('Coca Cola', 'The Coca-Cola Company', 224.99, 560, '2022-12-05', '2023-03-05');
+
+INSERT INTO Products(name, brand, price, stock, entryDate)
+	VALUES ('Fernet', 'Fratelli Branca', 314.99, 237, '2022-12-14');
 
 
+/* --------------------------- Comprobar Registros -------------------------- */
 
-/* --------------------------- Consultas Avanzadas -------------------------- */
+SELECT * FROM Employees;
 
-DESCRIBE Article; -- Muestra la estructura de la tabla 'Article'.
-
-SELECT * FROM Article; -- Obtiene todos los registros de la tabla 'Article'.
-SELECT * FROM Article WHERE ID = 2; -- Obtiene el registro que tiene el 'ID' = 2 de la tabla 'Article'.
-SELECT * FROM Music JOIN Article ON Music.articleID = Article.ID; -- Obtiene los registros compartidos entre la tabla 'Music' y 'Article', en base a los IDs.
-
--- Obtener registros compartidos en base a filtros
-SELECT
-	Article.ID,
-	Article.author,
-	Music.theCode
-FROM Music JOIN Article ON
-	Music.articleID = Article.ID
-JOIN MusicThemes ON
-	MusicThemes.musicID = Music.ID;
+SELECT * FROM Products;
