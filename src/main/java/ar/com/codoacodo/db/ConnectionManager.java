43e9cca2e957550d117d8b67ded9187b0ebe42ee
hozlucas28 <hozlucas28@gmail.com>
2022-12-23 18:42:25 -0300

    Create class for initialize database connection

---
+++ src/main/java/ar/com/codoacodo/db/ConnectionManager.java
@@ -0,0 +1,33 @@
+package ar.com.codoacodo.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	public static Connection initConnection() {
+		String host = "localhost";
+		String port = "3306";
+		String dbName = "webapp_practice";
+		String userName = "root";
+		String password = "289137tx";
+
+		String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?serverTimeZone=UTC&useSSL=false";
+		String driverClassname = "com.mysql.cj.jdbc.Driver";
+
+		try {
+			Class.forName(driverClassname);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(url, userName, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
