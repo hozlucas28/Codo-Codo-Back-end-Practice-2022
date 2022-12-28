2f08a46f1f25ad922dc0e5e4050045b73c88d4c3
hozlucas28 <hozlucas28@gmail.com>
2022-12-27 23:25:00 -0300

    Update 'ProductDAOImpl.jsp' interface implemation

--- src/main/java/ar/com/codoacodo/dao/impl/ProductDAOImpl.java
+++ src/main/java/ar/com/codoacodo/dao/impl/ProductDAOImpl.java
@@ -27,19 +27,35 @@ public class ProductDAOImpl implements IProductDAO {
 		statement.execute();
 	}
 
-	public void delete(int id) throws SQLException {
+	public boolean delete(int ID) throws SQLException {
 		var connection = ConnectionManager.initConnection();
 		Statement statement = connection.createStatement();
 
-		String query = "DELETE FROM Products WHERE id = " + id;
-		statement.executeUpdate(query);
+		String query = "SELECT * FROM Products WHERE ID = " + ID + ";";
+		ResultSet resultSet = statement.executeQuery(query);
+
+		if (resultSet.next()) {
+			query = "DELETE FROM Products WHERE ID = " + ID + ";";
+			statement.executeUpdate(query);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void deleteAll() throws SQLException {
+		var connection = ConnectionManager.initConnection();
+		Statement statement = connection.createStatement();
+
+		String query = "TRUNCATE TABLE Products;";
+		statement.execute(query);
 	}
 
 	public void update(Product product) throws SQLException {
 		var connection = ConnectionManager.initConnection();
 
 		String query =
-			"UPDATE Products SET name = ?, brand = ?, price = ?, stock = ?, entryDate = ?, expireDate = ?  WHERE id = ?";
+			"UPDATE Products SET name = ?, brand = ?, price = ?, stock = ?, entryDate = ?, expireDate = ?  WHERE ID = ?;";
 		PreparedStatement statement = connection.prepareStatement(query);
 
 		statement.setString(1, product.getName());
@@ -48,36 +64,19 @@ public class ProductDAOImpl implements IProductDAO {
 		statement.setInt(4, product.getStock());
 		statement.setString(5, product.getEntryDate());
 		statement.setString(6, product.getExpireDate());
+		statement.setInt(7, product.getID());
 		statement.execute();
 	}
 
-	public Product getByID(int id) throws SQLException {
+	public Product getByID(int ID) throws SQLException {
 		var connection = ConnectionManager.initConnection();
 		Statement statement = connection.createStatement();
 
-		String query = "SELECT * FROM Products WHERE id = " + id;
+		String query = "SELECT * FROM Products WHERE ID = " + ID + ";";
 		ResultSet resultSet = statement.executeQuery(query);
 
-		String name = resultSet.getString("name");
-		String brand = resultSet.getString("brand");
-		float price = resultSet.getFloat("price");
-		int stock = resultSet.getInt("stock");
-		String entryDate = resultSet.getString("entryDate");
-		String expireDate = resultSet.getString("expireDate");
-
-		return new Product(name, brand, price, stock, entryDate, expireDate);
-	}
-
-	public List<Product> getAll() throws SQLException {
-		var connection = ConnectionManager.initConnection();
-		Statement statement = connection.createStatement();
-
-		String query = "SELECT * FROM Products";
-		ResultSet resultSet = statement.executeQuery(query);
-
-		List<Product> products = new ArrayList<>();
-		while (resultSet.next()) {
-			int id = resultSet.getInt("id");
+		Product product = null;
+		if (resultSet.next()) {
 			String name = resultSet.getString("name");
 			String brand = resultSet.getString("brand");
 			float price = resultSet.getFloat("price");
@@ -85,7 +84,52 @@ public class ProductDAOImpl implements IProductDAO {
 			String entryDate = resultSet.getString("entryDate");
 			String expireDate = resultSet.getString("expireDate");
 
-			var product = new Product(id, name, brand, price, stock, entryDate, expireDate);
+			product = new Product(ID, name, brand, price, stock, entryDate, expireDate);
+		}
+		return product;
+	}
+
+	public List<Product> getAllByBrand(String brandToSearch) throws SQLException {
+		var connection = ConnectionManager.initConnection();
+		Statement statement = connection.createStatement();
+
+		String query = "SELECT * FROM Products WHERE brand = '" + brandToSearch + "';";
+		ResultSet resultSet = statement.executeQuery(query);
+
+		List<Product> products = new ArrayList<>();
+		while (resultSet.next()) {
+			int ID = resultSet.getInt("ID");
+			String name = resultSet.getString("name");
+			String brand = resultSet.getString("brand");
+			float price = resultSet.getFloat("price");
+			int stock = resultSet.getInt("stock");
+			String entryDate = resultSet.getString("entryDate");
+			String expireDate = resultSet.getString("expireDate");
+
+			var product = new Product(ID, name, brand, price, stock, entryDate, expireDate);
+			products.add(product);
+		}
+		return products;
+	}
+
+	public List<Product> getAll() throws SQLException {
+		var connection = ConnectionManager.initConnection();
+		Statement statement = connection.createStatement();
+
+		String query = "SELECT * FROM Products;";
+		ResultSet resultSet = statement.executeQuery(query);
+
+		List<Product> products = new ArrayList<>();
+		while (resultSet.next()) {
+			int ID = resultSet.getInt("ID");
+			String name = resultSet.getString("name");
+			String brand = resultSet.getString("brand");
+			float price = resultSet.getFloat("price");
+			int stock = resultSet.getInt("stock");
+			String entryDate = resultSet.getString("entryDate");
+			String expireDate = resultSet.getString("expireDate");
+
+			var product = new Product(ID, name, brand, price, stock, entryDate, expireDate);
 			products.add(product);
 		}
 		return products;
